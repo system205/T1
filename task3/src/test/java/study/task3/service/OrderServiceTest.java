80799c9ba34c4257ab7c0a3c64855bc5bf5aa661
system205 <arsenmutalapov205@gmail.com>
2024-03-26 08:42:54 +0300

    add testing of throwing and error

--- task3/src/test/java/study/task3/service/OrderServiceTest.java
+++ task3/src/test/java/study/task3/service/OrderServiceTest.java
@@ -1,15 +1,15 @@
 package study.task3.service;
 
-import jakarta.annotation.PostConstruct;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
+import study.task3.entity.Order;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 @ExtendWith(OutputCaptureExtension.class)
@@ -19,17 +19,26 @@ class OrderServiceTest {
     @Autowired
     private UserService userService;
 
-    @PostConstruct
-    void do1(){
+    @BeforeEach
+    void addUser(){
         userService.createUser("John", "john@gmail.com");
     }
 
     @Test
     void testCreateOrder(CapturedOutput output){
 
-        orderService.createOrder("test", 1L);
+        final Order order = orderService.createOrder("test", 1L);
+        assertNotNull(order.getId());
         assertEquals(1, orderService.getAllOrders().size());
 
         assertTrue(output.getOut().contains("Method class study.task3.service.OrderService.createOrder is called"));
+
+        assertThrows(IllegalArgumentException.class, () -> orderService.getOrder(123L));
+        assertTrue(output.getOut().contains("Method getOrder throws an exception: java.lang.IllegalArgumentException"));
+    }
+
+    @AfterEach
+    void removeUser(){
+        userService.deleteUser(1L);
     }
 }
